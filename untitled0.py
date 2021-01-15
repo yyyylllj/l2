6ded23fc97733ab5e6f006454eae8e4d0c9e77fe
yyyylllj <756905045@qq.com>
2021-01-15 16:04:15 +0800

    mnist

---
+++ untitled0.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 15:32:15 2021
+
+@author: ylj和xqq
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torchvision
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import cv2
+train_dataset = datasets.MNIST(root='./data/',train=True,transform=transforms.ToTensor(),download=True)
+test_dataset = datasets.MNIST(root='./data/',train=False,transform=transforms.ToTensor(),download=True)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=64,
+                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=64,
+                                          shuffle=False)
+class Model(nn.Module):
+    def __init__(self):
+        super(Model,self).__init__()
+        self.l11=nn.Linear(784,784)
+        self.l2=nn.Linear(784,784)
+        self.linear2=nn.Linear(784,10)
+    def forward(self,x):
+        x=F.relu(self.l11(x))
+        x=F.relu(self.l2(x))
+        x=self.linear2(x)
+        return x
+mod=Model()
+mod=mod.cuda()
+loss=nn.CrossEntropyLoss()
+optimizer=optim.SGD(mod.parameters(),lr=0.03)
+losses=[]
+acces=[]
+eval_losses=[]
+eval_acces=[]
+def func(a,b):
+    if torch.norm(a)>b:
+        c=a/torch.norm(a)*b
+        return(c)
+    else:
+        c=a
+        return(c)
+params=list(mod.parameters())
+for i in range(50):
+    train_loss=0
+    train_acc=0
+    mod.train()
+    for j,(X,label) in enumerate(train_loader):
+        X=X.view(-1,784)
+        X=Variable(X)
+        label=Variable(label)
+        X=X.cuda()
+        label=label.cuda()
+        out=mod(X)
+        lossvalue=loss(out,label)
+        optimizer.zero_grad()
+        lossvalue.backward()
+        optimizer.step()
+        for ll in range(784):
+            with torch.no_grad():
+                n=params[0][ll,:]
+                params[0][ll,:]=func(n,0.2)
+                m=params[0][ll,:]
+                params[2][ll,:]=func(m,0.2)
+        train_loss+=float(lossvalue)
+        _,pred=out.max(1)
+        num_correct=(pred==label).sum()
+        acc=int(num_correct)/X.shape[0]
+        train_acc+=acc
+    losses.append(train_loss/len(train_loader))
+    acces.append(train_acc/len(train_loader))
+    print("i:"+' ' +str(i))
+    print("lose:" + ' ' + str(train_loss / len(train_loader)))
+    print("accuracy:" + ' '+str(train_acc / len(train_loader)))
