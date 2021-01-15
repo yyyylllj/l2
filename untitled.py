6997d81b5d3e2b62c0e1e565b87cb81aa95f4b9b
yyyylllj <756905045@qq.com>
2021-01-15 16:20:18 +0800

    c10

---
+++ untitled.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 09:40:25 2020
+
+@author: lenovo
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 15:46:34 2020
+
+@author: lenovo
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 18:40:05 2020
+
+@author: lenovo
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torch
+
+
+class LeNet(nn.Module):
+
+    def __init__(self):
+        super(LeNet, self).__init__()
+
+        self.fc1 = nn.Linear(32 * 32 * 3, 32 * 32 * 2)
+        self.fc2 = nn.Linear(32 * 32 * 2, 32 * 32)
+        self.fc3 = nn.Linear(32 * 32, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+def func(a,b):
+    if torch.norm(a)>b:
+        c=a/torch.norm(a)*b
+        return(c)
+    else:
+        c=a
+        return(c)
+
+net = LeNet()
+net = net.cuda()
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
+train_set = torchvision.datasets.CIFAR10(root="./data",
+                                         train=True, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set,
+                                           batch_size=400, shuffle=True, num_workers=2)
+test_set = torchvision.datasets.CIFAR10(root="./data",
+                                        train=False, download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(train_set,
+                                          batch_size=400, shuffle=False, num_workers=2)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001)
+params=list(net.parameters())
+for epoch in range(20):
+    running_loss = 0
+    for i, data in enumerate(train_loader):
+        net.zero_grad()
+        inputs, labels = data
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+        inputs = inputs.view(-1, 32 * 32 * 3)
+        output = net(inputs)
+        train_loss = criterion(output, labels)
+        train_loss.backward()
+        optimizer.step()
+        for ll in range(32*32*2):
+            with torch.no_grad():
+                n=params[0][ll,:]
+                params[0][ll,:]=func(n,1)
+        for ll in range(32*32):
+            with torch.no_grad():
+                n=params[2][ll,:]
+                params[2][ll,:]=func(n,1)
+        running_loss += train_loss.data
+     
